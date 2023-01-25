ab6b5f649fb6404c8752328f26667c306d7d3da4
joyumoekpo <joyumoekpo1@gmail.com>
2023-01-25 14:22:01 -0600

    Fixed icon and button stylings in header and footer

--- src/components/RecipeCardComponents/RecipeCard.jsx
+++ src/components/RecipeCardComponents/RecipeCard.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import styles from "./RecipeCard.module.css";
-import { useNavigate } from "react-router-dom";
 
-const RecipeCard = ({recipe}) => {
-  const navigate = useNavigate()
-
-  const handleClick = () => {
-    navigate(`/recipe/${recipe.recipe_id}`)
-  }
-  return (
-    <div className={styles.recipe_card}>
-      <div>
-        <div className={styles.recipe_img_container}>
-          <img src={recipe.image_url} />
-        </div>
-        <h3>{recipe.recipe_name}</h3>
-      </div>
-      <button className="blue-btn" onClick={handleClick}>See More</button>
-    </div>
-  );
+const RecipeCard = () => {
+	return (
+		<div className={styles.recipe_card}>
+			<div>
+				<div className={styles.recipe_img_container}>
+					{/* <img src="" > */}
+				</div>
+				<h3>Chocolate Cake</h3>
+			</div>
+			<button className="blue-btn">
+				See More
+			</button>
+		</div>
+	);
 };
 
 export default RecipeCard;
--- src/components/homeComponents/HomeScreen.jsx
+++ src/components/homeComponents/HomeScreen.jsx
@@ -1,31 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-import AdBanner from './AdBanner'
+import AdBanner from "./AdBanner";
+import RecipeCard from "../RecipeCardComponents/RecipeCard";
 
 const HomeScreen = () => {
-  const [recipes, setRecipes] = useState([]);
-  
-  const getRecipes = () => {
-    axios
-        .get("https://recipes.devmountain.com/recipes")
-        .then((res) => {
-            setRecipes(res.data)
-            console.log(res.data)
-        })
-}
+	const [recipes, setRecipes] = useState([]);
 
-useEffect(() => {
-    getRecipes()
-},[])
+	const getRecipes = () => {
+		axios.get("https://recipes.devmountain.com/recipes").then((res) => {
+			setRecipes(res.data);
+			console.log(res.data);
+		});
+	};
 
+	useEffect(() => {
+		getRecipes();
+	}, []);
 
-  return (
-    <div>
-      <AdBanner />
-      {/* Much code from Part 2 will be placed around here. Do your best! */}
-    </div>
-  )
-}
+	return (
+		<div>
+			<AdBanner />
+			<RecipeCard />
+		</div>
+	);
+};
 
-export default HomeScreen
+export default HomeScreen;
