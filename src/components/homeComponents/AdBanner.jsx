6f05a444dd3f077406d9d885b0e818ea898100a8
joyumoekpo <joyumoekpo1@gmail.com>
2023-01-25 10:32:49 -0600

    Edited AdBanner styling

--- src/components/homeComponents/AdBanner.jsx
+++ src/components/homeComponents/AdBanner.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import salmon from "../../assets/salmon.jpg";
 import { Link } from "react-router-dom";
 
-import styles from "./Home.module.css";
+import styles from "./AdBanner.module.css";
 
 const AdBanner = () => {
   return (
     <div
-    className={styles.ad_banner}
+    className={styles.banner}
       style={{
-        
         background: `linear-gradient(
           190deg,
           rgba(0, 0, 0, 0.8),
@@ -18,7 +17,7 @@ const AdBanner = () => {
         backgroundSize: "cover",
       }}
     >
-      <div className={styles.ad_text}>
+      <div className={styles.banner_text}>
         <h3>New Recipe</h3>
         <h1>Pineapple Salmon</h1>
         <h3>
@@ -26,7 +25,7 @@ const AdBanner = () => {
           brown sugar rub, baked for 25 minutes on a bed of pineapple, and garnished in butter, garlic, and chives. You won’t want to miss it!
         </h3>
         <Link to="/recipe/3">
-          <button className="blue-btn">Check it out</button>
+          <button className={styles.banner_button}>Check it out</button>
         </Link>
       </div>
     </div>
