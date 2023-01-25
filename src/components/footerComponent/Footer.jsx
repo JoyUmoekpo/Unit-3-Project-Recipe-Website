e881c1af78d592252a3951a08009edf1cc3308b5
joyumoekpo <joyumoekpo1@gmail.com>
2023-01-25 09:40:40 -0600

    Edited footer styling

--- src/components/footerComponent/Footer.jsx
+++ src/components/footerComponent/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ImFacebook, ImInstagram, ImTwitter, ImGithub } from "react-icons/im";
+import { ImFacebook, ImInstagram, ImTwitter, ImGithub, ImLinkedin } from "react-icons/im";
 
 import styles from "./Footer.module.css"
 
