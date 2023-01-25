84cd7205c8d888f866623f54bc55aa34b77fae86
joyumoekpo <joyumoekpo1@gmail.com>
2023-01-25 11:15:02 -0600

    Removed hover effect on banner button

--- src/components/homeComponents/AdBanner.jsx
+++ src/components/homeComponents/AdBanner.jsx
@@ -5,31 +5,31 @@ import { Link } from "react-router-dom";
 import styles from "./AdBanner.module.css";
 
 const AdBanner = () => {
-  return (
-    <div
-    className={styles.banner}
-      style={{
-        background: `linear-gradient(
+	return (
+		<div
+			className={styles.banner}
+			style={{
+				background: `linear-gradient(
           190deg,
           rgba(0, 0, 0, 0.8),
           rgba(0, 0, 0, 0.8)),
           url(${salmon})`,
-        backgroundSize: "cover",
-      }}
-    >
-      <div className={styles.banner_text}>
-        <h3>New Recipe</h3>
-        <h1>Pineapple Salmon</h1>
-        <h3>
-          This recipe consists of fresh wild Alaskan salmon, rubbed in a bbq
-          brown sugar rub, baked for 25 minutes on a bed of pineapple, and garnished in butter, garlic, and chives. You won’t want to miss it!
-        </h3>
-        <Link to="/recipe/3">
-          <button className={styles.banner_button}>Check it out</button>
-        </Link>
-      </div>
-    </div>
-  );
+				backgroundSize: "cover",
+			}}>
+			<div className={styles.banner_text}>
+				<h3>New Recipe</h3>
+				<h1>Pineapple Salmon</h1>
+				<h3>
+					This recipe consists of fresh wild Alaskan salmon, rubbed in a bbq
+					brown sugar rub, baked for 25 minutes on a bed of pineapple, and
+					garnished in butter, garlic, and chives. You won’t want to miss it!
+				</h3>
+				<Link to="/recipe/3">
+					<button className={`${styles.banner_button} ${styles.disabled}`}>Check it out</button>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default AdBanner;
