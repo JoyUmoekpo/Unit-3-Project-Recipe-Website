ed1bb2ea7b5bba7ab173beb0aa67feb78116ec04
joyumoekpo <joyumoekpo1@gmail.com>
2023-01-25 18:15:36 -0600

    Edited RecipeCard Component

--- src/components/RecipeCardComponents/RecipeCard.jsx
+++ src/components/RecipeCardComponents/RecipeCard.jsx
@@ -3,14 +3,14 @@ import styles from "./RecipeCard.module.css";
 
 const RecipeCard = () => {
 	return (
-		<div className={styles.recipe_card}>
+		<div className={styles.card}>
 			<div>
-				<div className={styles.recipe_img_container}>
-					{/* <img src="" > */}
+				<div>
+					<img src="https://64.media.tumblr.com/01af5ca3719a6ecd8b51e804a1326c52/d8b1ed1c44da1425-a1/s400x600/53a367f515790543e80f219bdc37d8f5c273b29d.pnj" alt="Snack" className={styles.recipe_image}/>
 				</div>
-				<h3>Chocolate Cake</h3>
+				<h3 className={styles.recipe_text}>Chocolate Cake</h3>
 			</div>
-			<button className="blue-btn">
+			<button>
 				See More
 			</button>
 		</div>
