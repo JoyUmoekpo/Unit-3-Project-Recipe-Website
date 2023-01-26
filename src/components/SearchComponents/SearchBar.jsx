5e5b7fd49ff2ed78d3fdf2faf7a26ef78ede9280
joyumoekpo <joyumoekpo1@gmail.com>
2023-01-25 21:00:28 -0600

    Added state

--- src/components/SearchComponents/SearchBar.jsx
+++ src/components/SearchComponents/SearchBar.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiSearch } from "react-icons/bi";
 
 import styles from "./SearchBar.module.css";
 
 const Search = () => {
+	const [search, setSearch] = useState('');
+	
 	return (
 		<div className={styles.search}>
-			<BiSearch className={styles.search_icon}/>
+			<BiSearch className={styles.search_icon} />
 			<input
-				type="search"
-				name="search"
-				id="website-search"
+				type="text"
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
 				placeholder="Search for a Recipe"
 				className={styles.search_bar}
 			/>
