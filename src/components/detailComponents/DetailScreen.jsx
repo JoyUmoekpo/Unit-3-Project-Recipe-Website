63dfdf0ae9e36c251c558d170b88636804e467ff
joyumoekpo <joyumoekpo1@gmail.com>
2023-01-28 19:38:37 -0600

    Added template literal for useEffect link

--- src/components/detailComponents/DetailScreen.jsx
+++ src/components/detailComponents/DetailScreen.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import salmon from "../../assets/salmon.jpg";
+import styles from "./DetailScreen.module.css";
 
-const DetailScreen = () => {  
-  return (
-    <section>
-      {/* Welcome to the details page! This page will be reusable. Follow instructions to know what to do here. */}
-    </section>
-  );
+import axios from "axios";
+
+const DetailScreen = () => {
+	const { id } = useParams();
+	const [recipes, setRecipe] = useState({});
+
+	useEffect(() => {
+		axios.get(`https://recipes.devmountain.com/recipes/${id}`).then((res) => {
+			setRecipe(res.data);
+		});
+	}, []);
+
+	return (
+		<section>
+			<div
+				className={styles.detail_image}
+				style={{
+					background: `linear-gradient(
+          190deg,
+          rgba(0, 0, 0, 0.8),
+          rgba(0, 0, 0, 0.8)),
+          url(${salmon})`,
+					backgroundSize: "cover",
+				}}>
+				<h1>Pineapple Salmon</h1>
+			</div>
+			<div className={styles.details}>
+				<div className={styles.ingredients}>
+					<h2 className={styles.detail_text}>Recipe</h2>
+					<h4>Prep Time:</h4>
+					<h4>Cook Time:</h4>
+					<h4>Serves:</h4>
+					<br />
+					<h2 className={styles.detail_text}>Ingredients</h2>
+					{recipes.ingredients &&
+						recipes.ingredients.map((ing, index) => {
+							return (
+								<h4>
+									{ing.quantity} {ing.ingredient}
+								</h4>
+							);
+						})}
+				</div>
+				<div className={styles.instructions}>
+					<h2 className={styles.detail_text}>Instructions</h2>
+					<p style={{ whiteSpace: "pre-wrap" }}>
+						{recipes.instructions && JSON.parse(recipes.instructions)}
+					</p>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default DetailScreen;
